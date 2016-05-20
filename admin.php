3a41a8634888522da2e3d1059bd2efcf697d1e61
Viet Khang <vietkhangleho@gmail.com>
2016-05-21 01:10:57 +0200

    admin

--- admin.php
+++ admin.php
@@ -34,11 +34,53 @@
           </li>
           <li>
             <div class="collapsible-header"><i class="material-icons">supervisor_account</i>Liste du BDE</div>
-            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+            <div class="collapsible-body">            
+              <div class="collection">
+                  <a href="#!" class="collection-item">Membre 1</a>
+                  <a href="#!" class="collection-item">Membre 2</a>
+                  <a href="#!" class="collection-item">Membre 3</a>
+                  <a href="#!" class="collection-item">Membre 4</a>
+               </div>   
+            </div>
           </li>
           <li>
             <div class="collapsible-header"><i class="material-icons">play_circle_outline</i>Organiser un évènement</div>
-            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+            <div class="collapsible-body">
+              <div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">First Name</label>
+                  </div>
+                  <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Last Name</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+                    <label for="disabled">Disabled</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+
+            </div>
           </li>
         </ul>  
 
