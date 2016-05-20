b8c86f14bb8f3fb23bc235a951ff07e6bf075f19
Viet Khang <vietkhangleho@gmail.com>
2016-05-21 01:02:48 +0200

    admin

---
+++ admin.php
@@ -0,0 +1,58 @@
+       <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="UTF-8">
+            <!--Import Google Icon Font-->
+            <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <!--Import materialize.css-->
+            <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+            <!--Let browser know website is optimized for mobile-->
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          </head>
+
+          <body>
+            <!--Import jQuery before materialize.js-->
+            <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+            <script type="text/javascript" src="js/materialize.min.js"></script>
+
+            <header>
+                 <h1 class="center-align">Page Admin</h1>
+            </header> 
+
+             <ul class="collapsible" data-collapsible="accordion">
+          <li>
+            <div class="collapsible-header"><i class="material-icons">star_rate</i>Liste des soirées</div>
+            <div class="collapsible-body">
+              <div class="collection">
+                  <a href="#!" class="collection-item">Soirée 1</a>
+                  <a href="#!" class="collection-item">Soirée 2</a>
+                  <a href="#!" class="collection-item">Soirée 3</a>
+                  <a href="#!" class="collection-item">Soirée 4</a>
+               </div>   
+            </div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons">supervisor_account</i>Liste du BDE</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons">play_circle_outline</i>Organiser un évènement</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+          </li>
+        </ul>  
+
+
+            <footer class="page-footer">
+                <div class="container">
+                  <div class="row">
+                  </div>
+                </div>
+                <div class="footer-copyright">
+                  <div class="container">
+                     2016 Copyright
+                  </div>
+                </div>
+            </footer>
+          </body>
+        </html>
