822d8fcdf330841ef60c9729c6001866531d9713
Viet Khang <vietkhangleho@gmail.com>
2016-05-21 01:27:38 +0200

    add all

--- admin.php
+++ admin.php
@@ -45,41 +45,14 @@
           </li>
           <li>
             <div class="collapsible-header"><i class="material-icons">play_circle_outline</i>Organiser un évènement</div>
-            <div class="collapsible-body">
-              <div class="row">
-              <form class="col s12">
+              <div class="collapsible-body">
                 <div class="row">
-                  <div class="input-field col s6">
-                    <input id="first_name" type="text" class="validate">
-                    <label for="date">Date de l'évènement</label>
-                  </div>
-                  <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate">
-                    <label for="last_name">Last Name</label>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                    <label for="disabled">Disabled</label>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input id="password" type="password" class="validate">
-                    <label for="password">Password</label>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
-                    <label for="email">Email</label>
-                  </div>
-                </div>
-              </form>
-            </div>
-
-
+                <form class="col s12">
+                  <div class="row">
+                    <div class="input-field col s6">
+                      <input id="first_name" type="text" class="validate">
+                      <label for="date">Date de l'évènement</label>
+                    </div>
             </div>
           </li>
         </ul>  
