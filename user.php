6775d71eb11dd30a39ab679b46e334c914deb01d
Manel bk <Manel bk>
2016-05-21 03:32:18 +0200

    user

---
+++ user.php
@@ -0,0 +1,64 @@
+         <!DOCTYPE html>
+          <html>
+            <head>
+              <meta charset="UTF-8">
+              <!--Import Google Icon Font-->
+              <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+              <!--Import materialize.css-->
+              <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+              <!--Let browser know website is optimized for mobile-->
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </head>
+
+            <body>
+              <!--Import jQuery before materialize.js-->
+              <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+              <script type="text/javascript" src="js/materialize.min.js"></script>
+
+              <header>
+                <h1 class="center-align">Page User</h1>
+              </header> 
+
+              <ul class="collapsible popout" data-collapsible="accordion">
+              
+              <li>
+                <div class="collapsible-header"><i class="material-icons">filter_drama</i>Soirée 1</div>
+                <ul class="collapsible-body" data-collapsible="accordion">
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">filter_drama</i>Configuration 1</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">place</i>Configuration 2</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">whatshot</i>Configuration 3</div>
+                  <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+              </ul>
+              </li>
+              
+              <li>
+                <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+                <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+              </li>
+              
+              <li>
+                <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+                <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+              </li>
+            
+            </ul>
+              
+              <footer class="page-footer">
+                <div class="container">
+                  <div class="row"></div>
+                </div>
+                <div class="footer-copyright">
+                  <div class="container">2016 Copyright</div>
+                </div>
+              </footer>
+            </body>
+          </html>
